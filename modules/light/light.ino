e5b5241541b074c71240944196deae3feb7c95b9
Likkan Chung <llc721@student.bham.ac.uk>
2020-02-26 18:04:03 +0000

    Added delay timer

--- modules/light/light.ino
+++ modules/light/light.ino
@@ -1,7 +1,11 @@
 // this sketch reads a serial message and turns the light on if read
+// custom protocol "#[delay];[glow-time];\n"
 // using a custom protocol read the number of seconds <int> left to full brighness, from current, if -1 then off, if 0 then now
 
 const int led = 5;
+
+long delayTimer = 0;
+
 
 long timer = -1; // number of secs to go - starts the led off
 unsigned long startTime = 0;
@@ -11,7 +15,8 @@ int currentBrightness = 0;
 double gradient = 0;
 
 int inChar = 0;
-String inString = "";
+String partStr[] = {"",""};
+int part = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,32 +28,46 @@ void setup() {
 
 void loop() {
   while (Serial.available() > 0) {
-    startTime = millis();
-    startBrightness = currentBrightness;
     inChar = Serial.read();
-    if (isDigit(inChar) || inChar == '-') {
-      inString += (char)inChar;
-    }
 
+    if (inChar == '#') {
+      part = 0;
+    } else if (inChar == ';') {
+      part++;
+    } else {
+      if (isDigit(inChar) || inChar == '-') {
+        partStr[part] += (char)inChar;
+      }
+    }
     if (inChar == '\n') {
-      timer = inString.toInt();
-      endTime = startTime + (timer * 1000);
+      timer = partStr[1].toInt();
+      delayTimer = partStr[0].toInt();
+      endTime = startTime + (delayTimer * 1000) + (timer * 1000);
       gradient = (double)(255 - startBrightness) / (double)(timer * 1000);
-      inString = "";
+      startTime = millis() + (delayTimer * 1000);
+      startBrightness = currentBrightness;
+      Serial.print(delayTimer); Serial.print(" "); Serial.println(timer);
+      partStr[0] = "";
+      partStr[1] = "";
+      part = 0;
+
+      delayTimer = max(0, delayTimer);
     }
   }
 
-  if (timer == -1) {
-    currentBrightness = 0;
-  } else if (timer == 0) {
-    currentBrightness = 255;
-  } else {
-    // modelled as mx+c
-    currentBrightness = (int)(gradient * (millis() - startTime)) + startBrightness;  
-    currentBrightness = min(currentBrightness, 255);
-    currentBrightness = max(currentBrightness, 0);
-    
+  if (millis() >= startTime) {
+    if (timer == -1) {
+      currentBrightness = 0;
+    } else if (timer == 0) {
+      currentBrightness = 255;
+    } else {
+      // modelled as mx+c
+      currentBrightness = (int)(gradient * (millis() - startTime)) + startBrightness;  
+      currentBrightness = min(currentBrightness, 255);
+      currentBrightness = max(currentBrightness, 0);
+    }
   }
+  
   analogWrite(led, currentBrightness);
 
 }

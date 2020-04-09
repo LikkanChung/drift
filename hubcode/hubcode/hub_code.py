ceb1cfa86febb8444ec6af61c100624f46d39361
max <maxwarren@live.co.uk>
2020-04-09 02:28:59 +0100

    Connecting to login page

--- hubcode/hubcode/hub_code.py
+++ hubcode/hubcode/hub_code.py
@@ -1,11 +1,20 @@
 import serial
 import sys
+import time
+
+#The hub should
+#authenticate from the back end
+#Take alarms from the back end
+#activate light and sound when the alarm is received
+#deactivate afterwards
 
 def main(argv):
     print("Attempting connection")
     #if len(argv) != 2:
     #    print("Usage: " + argv[0] + "[COM port number]")
     arduino = serial.Serial('COM1', 9600, timeout=.1)
+    arduino.write("Welcome to drift")
+
 
 
 

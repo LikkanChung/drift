65ecca95f65848fc85c44fefdb4011ebc93a2de7
max <maxwarren@live.co.uk>
2020-04-09 05:36:52 +0100

    Fetching the Alarm times

--- hubcode/hubcode/backend_connection.py
+++ hubcode/hubcode/backend_connection.py
@@ -1,6 +1,7 @@
 import http.client
 import urllib.parse
 import urllib.request
+import json
 
 
 PORT = "3333"
@@ -8,23 +9,25 @@ TEST_USERNAME = "jack"
 TEST_PASSWORD =  "21ysxqkcl1i3c3h8ou7l"
 
 def fetch(server_address, username, password):
-    #connect
+    # Authorise
     print("connecting to: " + server_address + ":" + PORT)
     #http.client.HTTPconnection(server_address + ":" + PORT)
     values = {'username': username, 'password' : password}
     data = urllib.parse.urlencode(values)
     data = data.encode('utf-8')  # data should be bytes
-    req = urllib.request.Request(server_address + ":" + PORT + "/login", data)
-    resp = urllib.request.urlopen(req)
-    respData = resp.read()
-    print(respData)
-
-
-    #Authorise
-
+    login_req = urllib.request.Request(server_address + ":" + PORT + "/login", data)
+    login_resp = urllib.request.urlopen(login_req)
+    login_json = login_resp.read()
+    auth_data = json.loads(login_json)
+    token = auth_data["token"]
     #Fetch alarms
+    alarms_req = urllib.request.Request(server_address + ":" + PORT + "/alarms")
+    alarms_req.add_header("X-Auth-Token", token)
+    alarms_json = urllib.request.urlopen(alarms_req).read()
+    print("Alarms fetched:" + alarms_json)
     #format
     #return
+    return alarms_json
 
 
 if __name__ == "__main__":

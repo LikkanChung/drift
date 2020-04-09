463591426d376abaf0aeaa446f5234457de64121
max <maxwarren@live.co.uk>
2020-04-09 12:08:24 +0100

    Allowing for different COM Ports

--- hubcode/hubcode/hub_code.py
+++ hubcode/hubcode/hub_code.py
@@ -8,11 +8,16 @@ import time
 #activate light and sound when the alarm is received
 #deactivate afterwards
 
+display_module_port = 'COM1'
+light_module_port = 'COM2'
+sound_module_port = 'COM3'
+
+
 def main(argv):
     print("Attempting connection")
     #if len(argv) != 2:
     #    print("Usage: " + argv[0] + "[COM port number]")
-    arduino = serial.Serial('COM1', 9600, timeout=.1)
+    arduino = serial.Serial(display_module_port, 9600, timeout=.1)
     arduino.write("Welcome to drift")
 
 

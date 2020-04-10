a83986d5a356318520e38805cde6218d51c7bcf8
Likkan Chung <llc721@student.bham.ac.uk>
2020-04-10 17:45:44 +0100

    Merging

---
+++ AndroidApp/app/src/main/java/com/example/sleepyapps/AlarmsRecyclerAdapter.java
@@ -0,0 +1,91 @@
+package com.example.sleepyapps;
+
+import android.content.Context;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.sleepyapps.ui.alarms.AlarmsFragment;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+
+public class AlarmsRecyclerAdapter extends RecyclerView.Adapter<AlarmsRecyclerAdapter.ViewHolder> {
+
+    private static final String LOG_TAG = "AlarmsRecyclerAdapter";
+    private static final long ONE_DAY_IN_MILLIS = 1000 * 60 * 60 * 24;
+
+    private JSONArray data;
+    private Context myContext;
+    private AlarmsFragment clickListener;
+
+    public AlarmsRecyclerAdapter(JSONArray data,
+                                 AlarmsFragment clickListener) {
+        this.data = data;
+        this.clickListener = clickListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        myContext = parent.getContext();
+        View view = LayoutInflater.from(myContext)
+                .inflate(R.layout.alarms_recycler_item, parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        JSONObject alarm = data.optJSONObject(position);
+        String timeString;
+        if (alarm != null &&
+                (timeString = alarm.optString("time")) != null) {
+            try {
+                long time = Instant.parse(timeString).toEpochMilli();
+                holder.timeView.setText(DateUtils.formatDateTime(
+                        myContext, time, DateUtils.FORMAT_SHOW_TIME));
+                holder.dateView.setText(DateUtils.getRelativeDateTimeString(
+                        myContext, time, DateUtils.MINUTE_IN_MILLIS, ONE_DAY_IN_MILLIS,
+                        DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE));
+                holder.timeView.setOnClickListener((View v) -> {
+                    clickListener.onAlarmRecyclerItemTimeClick(alarm);
+                });
+                holder.dateView.setOnClickListener((View v) -> {
+                    clickListener.onAlarmRecyclerItemDateClick(alarm);
+                });
+            } catch (DateTimeParseException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.length();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public View itemView;
+        public TextView timeView;
+        public TextView dateView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.itemView = itemView;
+            timeView = itemView.findViewById(R.id.text_time);
+            dateView = itemView.findViewById(R.id.text_date);
+        }
+    }
+
+}
---
+++ AndroidApp/app/src/main/java/com/example/sleepyapps/PairingStage2Fragment.java
@@ -0,0 +1,31 @@
+package com.example.sleepyapps;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PairingStage2Fragment extends Fragment {
+
+
+    public PairingStage2Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pairing_stage2, container, false);
+    }
+}
